2607dab46a52b4ed5aee5c1c65c2d5bc4497e2b9
juampifortu <49237286+juampifortu@users.noreply.github.com>
2020-07-09 11:43:39 -0300

    Agrego barra de navegación secundaria

---
+++ inicioInternoViewAdmin.php
@@ -0,0 +1,56 @@
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+<main>
+    <div class="w3-bar w3-grey w3-card">
+            <a href="indexInterno?page=abmContenidistas" class="w3-bar-item w3-button">Gestionar Usuarios</a>
+            <a href="indexInterno?page=agregarNoticias" class="w3-bar-item w3-button">Agregar Noticias</a>
+            <a href="indexInterno?page=agregarPublicacion" class="w3-bar-item w3-button">Agregar Diario/Sección/Edición</a>
+            <a href="indexInterno?page=validarPublicacion" class="w3-bar-item w3-button">Publicaciones Pendientes</a>
+        </div>
+
+    <div align="center">
+        <h2 class="w3-wide w3-padding-16">Bienvenido Administrador</h2>
+
+        <!--<h3>¿Que puedo hacer actualmente?</h3>
+        <div class="w3-container w3-padding-32 ">
+            <a href="indexInterno?page=abmContenidistas" class="w3-button w3-round-xxlarge w3-blue-grey">Gestionar Usuarios</a>
+
+            <a href="indexInterno?page=agregarNoticias" class="w3-button w3-round-xxlarge w3-blue-grey">Agregar Noticias</a>
+
+            <a href="indexInterno?page=agregarPublicacion" class="w3-button w3-round-xxlarge w3-blue-grey">Agregar Diario/Sección/Edición</a>
+
+            <a href="indexInterno?page=validarPublicacion" class="w3-button w3-round-xxlarge w3-blue-grey">Publicaciones Pendientes</a>
+
+        </div>
+-->
+        <h3 class="w3-wide">Noticias Recientes</h3>
+
+        <div class=\"w3-row-padding w3-auto\">
+        <?php
+        foreach ($noticias as $noticia){
+            $id=$noticia['id_noticia'];
+            echo "
+        <a href='indexInterno?page=noticia&id=$id'>
+        <div class='w3-third'>
+            <p>" . strtoupper($noticia['titulo']) . "</p>"
+                . "<img src=" .$noticia['url_imagen'] ." width=200 height=120 >".
+                "</div>  
+        </a>
+        ";
+        }
+        ?>
+    </div>
+
+    </div>
+</main>
+
+</body>
+</html>
+
+
