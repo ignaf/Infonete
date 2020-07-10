8d12d59f808925e4ac0d2c233f0feed0bfd531ce
juampifortu <49237286+juampifortu@users.noreply.github.com>
2020-07-10 12:03:22 -0300

    Agrego los controller correspondientes

---
+++ controller/PublicacionExistenteController.php
@@ -0,0 +1,15 @@
+<?php
+
+class PublicacionExistenteController{
+
+    public function __construct()
+    {
+
+    }
+
+    public function execute(){
+        include_once ("view/publicacionExistenteView.php");
+    }
+
+}
+
---
+++ controller/PublicacionFallidaController.php
@@ -0,0 +1,15 @@
+<?php
+
+class PublicacionFallidaController{
+
+    public function __construct()
+    {
+
+    }
+
+    public function execute(){
+        include_once ("view/publicacionFallidaView.php");
+    }
+
+}
+
