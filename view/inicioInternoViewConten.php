13bb1e628b7fe8ca9ab49d61ec2c2e46f909ec59
ignaf <ignaa88@gmail.com>
2020-07-10 03:07:30 -0300

    fix estilo

--- view/inicioInternoViewConten.php
+++ view/inicioInternoViewConten.php
@@ -8,48 +8,47 @@
     <div align="center">
         <h2 class="w3-wide w3-padding-16">Bienvenido Contenidista</h2>
 
-       <!-- <h3>¿Que puedo hacer actualmente?</h3>
-        <div class="w3-container w3-padding-32 ">
-            <a href="indexInterno?page=agregarNoticias" class="w3-button w3-round-xxlarge w3-blue-grey">Agregar noticias</a>
-            <a href="indexInterno?page=agregarPublicacion" class="w3-button w3-round-xxlarge w3-blue-grey">Agregar diario/seccion/edicion</a>
-        </div>
--->
+        <div>
+            <h3 class="w3-wide">Noticias Recientes</h3>
 
-        <h3 class="w3-wide">Noticias Recientes</h3>
-
-        <div class=\"w3-row-padding w3-auto\">
-        <?php
-        foreach ($noticias as $noticia){
-            $id=$noticia['id_noticia'];
-            echo "
+            <div class="w3-row-padding w3-auto">
+                <?php
+                foreach ($noticias as $noticia){
+                    $id=$noticia['id_noticia'];
+                    echo "
+        <div>
         <a href='indexInterno?page=noticia&id=$id'>
         <div class='w3-third'>
             <p>" . strtoupper($noticia['titulo']) . "</p>"
-                . "<img src=" .$noticia['url_imagen'] ." width=200 height=120 >".
-                "</div>  
+                        . "<img src=" .$noticia['url_imagen'] ." width=200 height=120 >".
+                        "</div>  
         </a>
+        </div>
+        
         ";
-        }?>
+                }?>
+            </div>
+        </div>
 
-        <h3 class="w3-wide">Noticias Pendientes</h3>
+        <div>
+            <h3 class="w3-wide">Noticias Pendientes</h3>
 
-        <div class=\"w3-row-padding w3-auto\">
-        <?php
-        foreach ($noti as $noticia){
-            $id=$noticia['id_noticia'];
-            echo "
+            <div class="w3-row-padding w3-auto">
+            <?php
+            foreach ($noti as $noticia){
+                $id=$noticia['id_noticia'];
+                echo "
         <a href='indexInterno?page=validarPublicacion'>
         <div class='w3-third'>
             <p>" . strtoupper($noticia['titulo']) . "</p>"
-                . "<img src=" .$noticia['url_imagen'] ." width=200 height=120 >
+                    . "<img src=" .$noticia['url_imagen'] ." width=200 height=120 >
                 <i style='font-size:24px' class='fas'>&#xf06a;</i>".
-                "</div>
+                    "</div>
         </div>
         </a>
         ";
-        }?>
-    </div>
-
+            }?>
+        </div>
     </div>
 </main>
 
