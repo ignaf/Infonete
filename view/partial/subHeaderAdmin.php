66a61225da59d8a7d6f6feb2e3c3a771b582c9c1
Demian1701 <fioranidemian@gmail.com>
2020-07-12 03:41:43 -0300

    Cambio estilo subheaders
    Se cambia el background-color de los subheaders

--- view/partial/subHeaderAdmin.php
+++ view/partial/subHeaderAdmin.php
@@ -1,8 +1,9 @@
 <header>
-    <div class="w3-bar w3-grey w3-card">
+    <div class="w3-bar w3-card" style="background-color: #dadada">
         <a href="indexInterno?page=abmContenidistas" class="w3-bar-item w3-button">Gestionar Usuarios</a>
         <a href="indexInterno?page=agregarNoticias" class="w3-bar-item w3-button">Agregar Noticias</a>
         <a href="indexInterno?page=agregarPublicacion" class="w3-bar-item w3-button">Agregar Diario/Sección/Edición</a>
         <a href="indexInterno?page=validarPublicacion" class="w3-bar-item w3-button">Publicaciones Pendientes</a>
     </div>
 </header>
+
--- view/partial/subHeaderConten.php
+++ view/partial/subHeaderConten.php
@@ -1,5 +1,5 @@
 <header>
-    <div class="w3-bar w3-grey w3-card">
+    <div class="w3-bar w3-card" style="background-color: #dadada">
         <a href="indexInterno?page=agregarNoticias" class="w3-bar-item w3-button">Agregar Noticias</a>
         <a href="indexInterno?page=agregarPublicacion" class="w3-bar-item w3-button">Agregar Diario/Sección/Edición</a>
     </div>
